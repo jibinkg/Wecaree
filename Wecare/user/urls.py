409fb2a6ace3fb0412c04de2d04bde58a8e04332
jibinkg <jibinkoloth113@gmail.com>
2022-05-23 20:39:16 +0530

    all

--- Wecare/user/urls.py
+++ Wecare/user/urls.py
@@ -3,4 +3,13 @@ from .import views
 
 urlpatterns = [
     path('',views.home),
+    path('sp',views.special),
+    path('dc',views.doctor),
+    path('book', views.booking),
+    path('rcp',views.reciept),
+    path('log', views.login),
+    path('sup', views.signup),
+
+
+
 ]
--- Wecare/user/views.py
+++ Wecare/user/views.py
@@ -3,3 +3,21 @@ from django.http import HttpResponse
 # Create your views here.
 def home(re):
     return render(re,'user/mainpage.html')
+
+def special(re):
+    return render(re,'user/specialization.html')
+
+def doctor(re):
+    return render(re,'user/doctorslist.html')
+
+def booking(re):
+    return render(re,'user/bookingform.html')
+
+def reciept(re):
+    return render(re,'user/reciept.html')
+
+def login(re):
+    return render(re,'user/registration/signin.html')
+
+def signup(re):
+    return render(re,'user/registration/signup.html')
